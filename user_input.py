e6ef2858b2ebbd9afd95365008af8bf5476aa70f
John Kenechukwu Okoye <93558021+johnkoye19@users.noreply.github.com>
2024-10-30 17:35:33 +0100

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# Program to take a set of inputs and return an output based on the inputs
+
+# inputs
+name = input('What is your name?')
+age = int(input('How old are you?'))
+location = input('Where are you?')
+
+
+# output
+print(f'Hello, {name}. You are {age} years old and you are in {location}')
